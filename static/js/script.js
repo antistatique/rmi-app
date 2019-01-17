37889f863b48e0e1397edceaa9acd80b1b49c20f
Pierre De Pasquale <pierre.depasquale@responsibleminingindex.org>
2019-01-17 11:11:05 +0000

    script.js edited online with Bitbucket (added table sorting function)

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,28 @@ function expand_collapse(id) {
     g.style.color = '#000000';
   }
 }
+
+function mySortFunction() {
+  // Declare variables 
+  var input, filter, table, tr, td, td2, i, txtValue, txtValue2;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+
+for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    td2 = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      txtValue2 = td2.textContent || td2.innerText;
+      if ((txtValue.toUpperCase().indexOf(filter) > -1) || (txtValue2.toUpperCase().indexOf(filter) > -1)){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
