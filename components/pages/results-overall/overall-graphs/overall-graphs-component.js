d55287ff96b25df78d4a8b8a799761df5652202d
Steven Avelino <steven.avelino24@outlook.com>
2019-08-07 11:49:31 +0200

    Get current language from props to pass to graphs

--- components/pages/results-overall/overall-graphs/overall-graphs-component.js
+++ components/pages/results-overall/overall-graphs/overall-graphs-component.js
@@ -11,16 +11,19 @@ import { GRAPHS_PER_ROW } from './overall-graphs-constants';
 import styles from './overall-graphs-styles.scss';
 
 class OverallGraphs extends PureComponent {
-  static propTypes = { data: PropTypes.array.isRequired }
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    currentLanguage: PropTypes.string.isRequired
+  }
 
-  static renderGraphRow(graphs, key) {
+  static renderGraphRow(graphs, key, currentLanguage) {
     return (
       <Fragment key={key} >
         <style jsx>{styles}</style>
         <div className="row">
           {graphs.map(graph => (
             <div key={graph.id} className="col-xs-12 col-md-4">
-              <OverallGraphsItem data={graph} />
+              <OverallGraphsItem data={graph} currentLanguage={currentLanguage} />
             </div>
           ))}
         </div>
@@ -29,7 +32,7 @@ class OverallGraphs extends PureComponent {
   }
 
   renderGraphs() {
-    const { data } = this.props;
+    const { data, currentLanguage } = this.props;
     const totalRows = (data.length / GRAPHS_PER_ROW) > parseInt(data.length / GRAPHS_PER_ROW, 10) ?
       parseInt(data.length / GRAPHS_PER_ROW, 10) + 1 : parseInt(data.length / GRAPHS_PER_ROW, 10);
     const graphs = [];
@@ -37,7 +40,7 @@ class OverallGraphs extends PureComponent {
     for (let i = 0; i < totalRows; i++) {
       const limit = ((i * GRAPHS_PER_ROW) + GRAPHS_PER_ROW);
       const slicedGraphs = data.slice(i * GRAPHS_PER_ROW, limit);
-      graphs.push(OverallGraphs.renderGraphRow(slicedGraphs, i));
+      graphs.push(OverallGraphs.renderGraphRow(slicedGraphs, i, currentLanguage));
     }
 
     return graphs;
@@ -49,14 +52,6 @@ class OverallGraphs extends PureComponent {
         <style jsx>{styles}</style>
         <div className="graph-container">
           {this.renderGraphs()}
-        </div>
-        <div className="explanation">
-          <p><span className="aggregation-line" />Current Best Practice: Aggregation of best scores for all indicators in the given thematic area.</p>
-          <p>The &apos;Current Best Practice&apos; value represents the aggregation of best
-            scores achieved for all indicators in the given thematic area, taking into
-            account all companies&apos; results.<br /> The maximum value of 6.0
-            represents the maximum achievable score.
-          </p>
         </div>
       </div>
     );
