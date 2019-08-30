efdb5e3d0ecb66f3c48e989ea19378affe68aaa1
Yann Lugrin <yann.lugrin@antistatique.net>
2019-08-30 16:09:31 +0200

    Setup deploy

---
+++ config/deploy/staging.rb
@@ -0,0 +1,7 @@
+set :branch, :deploy
+set :deploy_to, '/data/docker-data/2020.responsibleminingindex.org/front'
+
+server '83.166.150.184', port: '54698', user: 'antistatique', roles: %w{app db web}
+
+SSHKit.config.command_map[:docker_compose] = "sudo docker-compose -f #{current_path.join('docker-compose.yml')} -f #{current_path.join('docker-compose.prod.yml')} -p #{fetch(:docker_app_name)}"
+SSHKit.config.command_map[:docker] = 'sudo docker'
