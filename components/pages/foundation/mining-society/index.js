b528a00056750ffe6918b35ad46f2b36e9d0b363
Steven Avelino <steven.avelino24@outlook.com>
2019-09-12 16:09:43 +0200

    Remove all files for foundation

--- components/pages/foundation/mining-society/index.js
+++
@@ -1,9 +0,0 @@
-// redux
-import { connect } from 'react-redux';
-
-import MiningSocietyPage from './mining-society-component';
-
-export default connect(
-  state => ({ content: state.staticContent.content }),
-  {}
-)(MiningSocietyPage);
--- components/pages/foundation/our-work/index.js
+++
@@ -1,9 +0,0 @@
-// redux
-import { connect } from 'react-redux';
-
-import OurWorkPage from './our-work-component';
-
-export default connect(
-  state => ({ content: state.staticContent.content }),
-  {}
-)(OurWorkPage);
