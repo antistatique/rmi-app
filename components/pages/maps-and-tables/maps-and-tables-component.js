bdb7d6f0bfacaa6ba10a8ddd5dae0ff009e963ed
Steven Avelino <steven.avelino24@outlook.com>
2019-09-19 09:29:42 +0200

    Create page component for maps and tables

---
+++ components/pages/maps-and-tables/maps-and-tables-component.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+import MapTaxJurisdictions from './map-known-tax-jurisdictions';
+
+class MapsAndTables extends PureComponent {
+  render() {
+    return (
+      <MapTaxJurisdictions />
+    )
+  }
+}
+
+export default MapsAndTables;
