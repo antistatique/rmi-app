e2951faf4534cf29d9b7fe18c65880111defea7b
Julien Minguely <jminguely@gmail.com>
2019-10-02 12:04:04 +0200

    Better tooltip placement & optimization on mobile

--- components/common/companies-list/companies-list-item.js
+++ components/common/companies-list/companies-list-item.js
@@ -100,14 +100,13 @@ class CompaniesListItem extends PureComponent {
           targetAttachment="bottom center"
           className="companies-list-tooltip-tether"
           key={id}
-          constraints={[{
-            to: 'target',
-            attachment: 'together'
-          }, {
-            to: 'window',
-            attachment: 'target',
-            pin: true
-          }]}
+          constraints={[
+            {
+              to: 'scrollParent',
+              attachment: 'together none',
+              pin: true
+            }
+          ]}
         >
           <div
             className="companies-list-item"
