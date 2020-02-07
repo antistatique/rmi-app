77d524a002e2aa438e7cad0266e69d146f1b7946
Steven Avelino <steven.avelino24@outlook.com>
2020-02-07 15:20:50 +0100

    Add portuguese

--- components/layout/header/language-bar/language-bar-constants.js
+++ components/layout/header/language-bar/language-bar-constants.js
@@ -1,3 +1,3 @@
-export const LANGUAGES_ORDER = ['zh', 'en', 'es', 'fr', 'id', 'ru'];
+export const LANGUAGES_ORDER = ['zh', 'en', 'es', 'fr', 'id', 'ru', 'pt'];
 
 export default { LANGUAGES_ORDER };
