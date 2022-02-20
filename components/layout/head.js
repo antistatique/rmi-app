b15d2fcaad7f058831f29f256c3b182829691c7f
Gilles Doge <gilles@antistatique.net>
2022-02-20 14:41:58 +0100

    update matomo script to use 2022 tracking

--- components/layout/head.js
+++ components/layout/head.js
@@ -83,7 +83,7 @@ class Head extends PureComponent {
               (function() {
                 var u="https://responsibleminingindex.innocraft.cloud/";
                 _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '3']);
+                _paq.push(['setSiteId', '4']);
                 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
                 g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.innocraft.cloud/responsibleminingindex.innocraft.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
               })();`
